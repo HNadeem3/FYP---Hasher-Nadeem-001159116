d61fb971d7d4e9fd958f79d5cc6fef43b04f7929
Hasher <hn6597a@gre.ac.uk>
2026-03-26 11:16:02 +0000

    Added API integration with minor adjustments still needed to be made

--- webapp.py
+++ webapp.py
@@ -1,4 +1,10 @@
 from flask import Flask
+from flask import jsonify
+#import os (Non-hardcoded version)
+#from dotenv import load_dotenv (Non-hardcoded version)
+import google.generativeai as genai
+
+
 # creates flash object
 app = Flask(__name__)
 
@@ -26,17 +32,27 @@ def ask_ai():
 # route for organisation policies and legal frameworks
 @app.route('/policies')
 def policies():
-    return "These are the policies to be aware of."
+    return "These are the policies to be aware of:"
 
 # route for keeping main goal in mind as a CISO within the Organisation
 @app.route('/CISOs_purpose')
-def CISOs_purpose():
-    return "These are the CISOs to be aware of."
+def ciso_purpose():
+    return "Your purpose during whilst working for the SME are:"
 
 # route for Organisation's assets (budget, email server security, customer details database, employee login credentials )
 @app.route('/assets/<category>')
 def assets(category):
-    return f"This is a list of all assets: {category}"
+    if category == "budget":
+        # Return budget data
+        return jsonify({
+            "category": "budget",
+            "data": {
+                "total_budget": 20000,
+            }
+        })
+    else:
+        return jsonify({"Error": "Invalid category selected"}),
+
 
 # route for threat vectors
 @app.route('/threats/<category>/<vector_name>')
@@ -46,7 +62,7 @@ def threats(category, vector_name):
 # route for incidents
 @app.route('/report/<incidents>')
 def report(incidents):
-    return f"This is a list of all incidents that have occurred."
+    return f"This is a list of all incidents that have occurred: {incidents}"
 
 #route for SME sector.
 @app.route('/industry/<sector>')
@@ -57,6 +73,36 @@ def industry_risks(sector):
 @app.route('/user/<name>')
 def show_user(name):
     return f"Hello, {name}! This is your personalised page."
+
+#API integration section: placeholder code for now
+'''
+#Non-hardcoded version:
+#loads API key from .env ( Environment Variables File )
+load_dotenv()
+
+genai.configure(api_key=os.getenv("Actual Key"))
+
+#hardcoded version
+api_key = "hardcoded api-key"
+# No need to import os or dotenv
+genai.configure(api_key="Actual Key")
+'''
+
+model = genai.GenerativeModel(
+    model_name="gemini-1.5-flash",
+    system_instructions = "You are a cyber security advisor supporting a CISO working for an SME in the EU."
+    " Your purpose is to help protect the SME from cyber security attacks and help them achieve their goals."
+    " Your role is to give strategic advice to reduce risks whilst also managing the organisation’s needs"
+    " and resource constraints. The main threat vectors you are dealing with are phishing, malware and email"
+    " vectors. The critical assets that need to be protected are the financial and customer data, user "
+    "login credentials and the core IT and operational systems. You also need to account for government"
+    " & organisational factors and policies. When giving advice, it should be practical, strategic"
+    " and actionable, mirroring how a real CISO would act in any given situation. Your solutions"
+    " should also strike a balance between cost effectivity and strong protection from the given threat"
+    " vectors. You should objectively evaluate any ideas the CISO brings to you and what alternatives"
+    " could be looked at or improvements made based on the qualities listed previously. You should support"
+    " the CISO and work within the given constraints, not expanding upon them."
+)
 
 # runs app and checks errors
 if __name__ == '__main__':
